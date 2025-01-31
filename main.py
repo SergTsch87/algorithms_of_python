89f4dbcf852cc220f6767d43ae38851eec4b99f7
SergTsch87 <sergtsch87@gmail.com>
2025-01-31 14:57:44 +0200

    Update: Update code in func bubble_sort, in main.py

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 #!usr/bin/env
 
 import random as rnd
-import math
+# import math
+
+
+def bubble_sort(arr):
+    len_arr = len(arr)
+    for i in range(len_arr):
+        swapped = False
+        for j in range(len_arr - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped: # якщо масив на якійсь ітерації вже є відсортованим, - тоді не витрачаймо ресурсів на зайві ітерації
+            break
+    return arr
 
 
 def main():
-    pass
+    # list_unsort = rnd.sample(range(1, 100), rnd.randint(10, 20))
+    list_unsort = [3, 7, 2, 9, 1, 0, 4, 8, 6, 5]
+
+    print(list_unsort)
+    print(bubble_sort(list_unsort))
+
 
 
 if __name__ == '__main__':
