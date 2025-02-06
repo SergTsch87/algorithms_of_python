674297327d6ff941876e1ad762c269013f5b1297
SergTsch87 <sergtsch87@gmail.com>
2025-02-06 21:23:07 +0200

    Update: Прибрав swapped у bubble_sort_recursive

--- bubble_sort.py
+++ bubble_sort.py
@@ -15,13 +15,13 @@ def bubble_sort_recursive(arr, n = None):
     if n == 1:
         return arr
 
-    swapped = False    
+    # swapped = False    
     for i in range(n - 1):
         if arr[i] > arr[i + 1]:
             arr[i], arr[i + 1] = arr[i + 1], arr[i]
-            swapped = True
+            # swapped = True
         
-    if not swapped:
-        return arr
+    # if not swapped:
+    #     return arr
         
     return bubble_sort_recursive(arr, n - 1)
