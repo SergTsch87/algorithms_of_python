417c801442e46564082b1fd22a8b48912a5a1e96
SergTsch87 <sergtsch87@gmail.com>
2025-02-09 00:35:15 +0200

    Upd, Test: Updating func recursive_selection_sort in selection_sort.py + Tests pass

--- selection_sort.py
+++ selection_sort.py
@@ -12,21 +12,19 @@ def selection_sort(arr):
     return arr
 
 
-def recursive_selection_sort(arr, n=None):
+def recursive_selection_sort(arr, n=None, start=0):
     if n is None:
         n = len(arr)
-        i = 0
-    
-    if n <= 1:
+        
+    if start >= n - 1:  # База рекурсії: коли залишився один елемент або масив порожній
         return arr
     
-    # for i in range(n - 1):
-    m_index = i
-    for j in range(i + 1, n):
-        if arr[j] < arr[m_index]:
-        m_index = j
+    # Знаходимо мінімальний елемент у підмасиві arr[start: ]
+    m_index = start
+    for i in range(start + 1, n):
+        if arr[i] < arr[m_index]:
+            m_index = i
     
-    arr[i], arr[m_index] = arr[m_index], arr[i]
-    i += 1
+    arr[start], arr[m_index] = arr[m_index], arr[start]
             
-    return recursive_selection_sort(arr, n-1)
+    return recursive_selection_sort(arr, n, start + 1)
