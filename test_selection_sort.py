2e68d7a5e841f84d48b55041b2b6115d35934028
SergTsch87 <sergtsch87@gmail.com>
2025-02-08 16:09:00 +0200

    Add: adding func test_sort_empty to new test_selection_sort.py

---
+++ test_selection_sort.py
@@ -0,0 +1,7 @@
+import pytest
+from bubble_sort import selection_sort   # Import the function (not implemented yet)
+
+
+# Test case 1: Sorting an empty list should return an empty list
+def test_sort_empty():
+    assert selection_sort([]) == []
