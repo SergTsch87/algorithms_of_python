9c73f431c3f30ebdc7efdcc83d66790aa9598857
SergTsch87 <sergtsch87@gmail.com>
2025-02-08 16:17:20 +0200

    Test, Upd: Error test; Adding funcs test_single_element + test_sort_two_elements to test_selection_sort.py

--- test_selection_sort.py
+++ test_selection_sort.py
@@ -5,3 +5,13 @@ from selection_sort import selection_sort   # Import the function (not implement
 # Test case 1: Sorting an empty list should return an empty list
 def test_sort_empty():
     assert selection_sort([]) == []
+
+
+# Test case 2: Sorting an empty list should return an empty list
+def test_single_element():
+    assert selection_sort([2]) == [2]
+
+
+# Test case 3: A list that is already sorted should remain the same
+def test_sort_two_elements():
+    assert selection_sort([9,4]) == [4,9]
