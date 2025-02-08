0f00c9a9815d5739672fe27613be61820cedfc9e
SergTsch87 <sergtsch87@gmail.com>
2025-02-08 12:18:35 +0200

    Add, Upd: Adding test_recursive_bubble_sort.py + Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,21 +7,25 @@ def bubble_sort(arr):
     return arr
 
 
-# Recursive version
-def bubble_sort_recursive(arr, n = None):
-    if n is None:
-        n = len(arr)
+# # Recursive version
+# def bubble_sort_recursive(arr, n = None):
+#     if n is None:
+#         n = len(arr)
 
-    if n == 1:
-        return arr
+#     if n == 1:
+#         return arr
 
-    # swapped = False    
-    for i in range(n - 1):
-        if arr[i] > arr[i + 1]:
-            arr[i], arr[i + 1] = arr[i + 1], arr[i]
-            # swapped = True
+#     # swapped = False    
+#     for i in range(n - 1):
+#         if arr[i] > arr[i + 1]:
+#             arr[i], arr[i + 1] = arr[i + 1], arr[i]
+#             # swapped = True
         
-    # if not swapped:
-    #     return arr
+#     # if not swapped:
+#     #     return arr
         
-    return bubble_sort_recursive(arr, n - 1)
+#     return bubble_sort_recursive(arr, n - 1)
+
+
+# def recursive_bubble_sort(arr):
+#     return arr
---
+++ test_recursive_bubble_sort.py
@@ -0,0 +1,8 @@
+import pytest
+from bubble_sort import bubble_sort   # Import the function (not implemented yet)
+import time
+
+
+# Test case 1: Sorting an empty list should return an empty list
+def test_sort_empty():
+    assert recursive_bubble_sort([]) == []
