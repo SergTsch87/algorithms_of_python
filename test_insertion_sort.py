d170e5e19623f678f96f446654e6e8a125ba6744
SergTsch87 <sergtsch87@gmail.com>
2025-02-17 12:51:03 +0200

    Upd: Add few funcs to test_insertion_sort.py

--- test_insertion_sort.py
+++ test_insertion_sort.py
@@ -2,14 +2,14 @@ import pytest
 from insertion_sort import insertion_sort   # Import the function (not implemented yet)
 
 
-# # Test case 1: Sorting an empty list should return an empty list
-# def test_empty_list():
-#     assert insertion_sort([]) == []
+# Test case 1: Sorting an empty list should return an empty list
+def test_empty_list():
+    assert insertion_sort([]) == []
 
 
-# # Test case 2: A single element list should remain unchanged
-# def test_single_element():
-#     assert insertion_sort([5]) == [5]
+# Test case 2: A single element list should remain unchanged
+def test_single_element():
+    assert insertion_sort([5]) == [5]
 
 
 # # Test case 2: A single element list should remain unchanged
@@ -18,9 +18,9 @@ from insertion_sort import insertion_sort   # Import the function (not implement
 #     # The function already returns the same list, so no change is needed.
 
 
-# def test_sort_two_elements():
-#     assert insertion_sort([2,1]) == [1,2]
-#     # Now we need logic to sort two elements.
+def test_sort_two_elements():
+    assert insertion_sort([2,1]) == [1,2]
+    # Now we need logic to sort two elements.
 
 
 # def test_sort_three_elements():
