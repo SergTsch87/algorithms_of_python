331db54922ea532fd49bbcc9b73d6b614d5b2ecc
SergTsch87 <sergtsch87@gmail.com>
2025-02-22 13:53:48 +0200

    Upd: add func merge + update func merge_sort

--- merge_sort.py
+++ merge_sort.py
@@ -1,23 +1,28 @@
-def merge_sort(arr: list[int]) -> list[int]:
-    # if not arr:
-    #     return arr
-    
-    n = len(arr)
-    if n <= 1:
+def merge_sort(arr):
+    if len(arr) <= 1:
         return arr
     
     mid = len(arr) // 2
 
-    # result = []
-    left_half = arr[:mid]
-    right_half = arr[mid:]
-
-    # result.extend(right_half)
-    # result.extend(left_half)
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
     
-    # return result
     return merge(left_half, right_half)
 
 
 def merge(left, right):
-    pass
+    result = []
+    i = j = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:  # Maintain stability
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:]) # Append remaining elements
+    result.extend(right[j:])
+    
+    return result
