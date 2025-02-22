a0870be25ee5e45a94d20dc1918240c4d22cb63f
SergTsch87 <sergtsch87@gmail.com>
2025-02-22 12:22:49 +0200

    Add, Upd, Test: before adding files test_merge_sort.py + merge_sort.py; add func test_empty_list to test_merge_sort.py + func merge_sort to merge_sort.py

--- test_merge_sort.py
+++ test_merge_sort.py
@@ -1,2 +1,15 @@
 import pytest
 from merge_sort import merge_sort   # Import the function (not implemented yet)
+
+
+# Test case 1: Sorting an empty list should return an empty list
+def test_empty_list():
+    assert merge_sort([]) == []
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    pytest.main()
